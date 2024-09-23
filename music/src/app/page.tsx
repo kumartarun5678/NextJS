d253bafea7c240b92dda3f6f7373074052780a66
Tarun Kumar <100301640+kumartarun5678@users.noreply.github.com>
2024-09-23 07:22:58 +0000

    Third

--- music/src/app/page.tsx
+++ music/src/app/page.tsx
@@ -1,9 +1,22 @@
+import Cards from "@/components/Cards";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import Scroll from "@/components/Scroll";
+import UpcomingWebinars from "@/components/UpComingWebnor";
+import Footer from "@/components/Footer";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-whte/[0.02]">
-      <h1 className="text-2xl text-center m-4">Hello India</h1>
+      <HeroSection/>
+      <FeaturedCourses/>
+      <Scroll/>
+      <Cards/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
